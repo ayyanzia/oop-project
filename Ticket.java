5fc82c48f72aebe116f096abe4f2d82be1ddb5a2
mikaal2003 <mikaalbutt08@gmail.com>
2024-11-23 19:56:32 +0500

    Add files via upload

---
+++ Ticket.java
@@ -0,0 +1,66 @@
+import java.time.LocalTime;
+import java.time.Duration;
+
+public class Ticket {
+    private String ticketID;
+    private Vehicle vehicle;
+    private ParkingSpot parkingSpot;
+    private LocalTime exitTime;
+
+    public Ticket(String ticketID, Vehicle vehicle, ParkingSpot parkingSpot) {
+        this.ticketID = ticketID;
+        this.vehicle = vehicle;
+        this.parkingSpot = parkingSpot;
+    }
+
+    public String getTicketID() {
+        return ticketID;
+    }
+
+    public void setTicketID(String ticketID) {
+        this.ticketID = ticketID;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public ParkingSpot getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public void setParkingSpot(ParkingSpot parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+
+    public void setExitTime(LocalTime exitTime){
+        this.exitTime = exitTime;
+    }
+
+    public LocalTime getExitTime(){
+        return exitTime;
+    }
+
+    public String calculateDuration() {
+        if (vehicle.getEntryTime()!= null && exitTime != null) {
+            Duration duration = Duration.between(vehicle.getEntryTime(), exitTime); // Use Vehicle's entry time
+            long hoursTime = duration.toHours();
+            long minutesTime = duration.toMinutes();
+            return hoursTime + "hours, " + minutesTime + " minutes." ;
+
+
+        } else {
+            throw new IllegalStateException("Entry or exit timestamp not set.");
+        }
+    }
+
+
+
+}
+
+
+
