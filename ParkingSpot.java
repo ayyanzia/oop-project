1ca74a213636036fbce8acf3ec23c6bae6912548
mikaal2003 <mikaalbutt08@gmail.com>
2024-12-11 23:32:28 +0500

    Update ParkingSpot.java

--- ParkingSpot.java
+++ ParkingSpot.java
@@ -1,11 +1,13 @@
 public class ParkingSpot {
     private String spotID; // unique identifier for a spot
     private Boolean occupancyStatus; // checks whether a spot is available
+    private String spotType;
 
 
-    public ParkingSpot(String spotID, Boolean occupancyStatus){
+    public ParkingSpot(String spotID, Boolean occupancyStatus,String spotType){
         this.spotID = spotID;
-        this.occupancyStatus = occupancyStatus;
+        this.occupancyStatus = false;
+        this.spotType = spotType;
     }
 
     public String getSpotID() {
@@ -23,4 +25,19 @@ public class ParkingSpot {
     public void setOccupancyStatus(Boolean occupancyStatus) {
         this.occupancyStatus = occupancyStatus;
     }
+
+    public String getSpotType() {
+        return spotType;
+    }
+
+    public void setSpotType(String spotType) {
+        this.spotType = spotType;
+    }
+
+    public boolean isCompatibleWith(Vehicle vehicle){
+        return this.spotType.equalsIgnoreCase(vehicle.getVehicleType());
+    }
+
+
+
 }
